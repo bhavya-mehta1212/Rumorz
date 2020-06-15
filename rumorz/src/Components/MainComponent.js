7bc76b8d0d8fc999e2ddd826fb8ba0f4746ac405
Bhavya Mitesh Mehta <bhmitesh@microsoft.com>
2020-06-15 16:07:41 +0530

    Added Create rumour component

---
+++ rumorz/src/Components/MainComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container,Row} from 'react-bootstrap';
+import CreateRumour from './CreateRumour';
+import './Styling/MainComponent.css'
+function MainComponent() {
+  return(
+    <Container fluid style={{paddingLeft:'20px',paddingTop:'5px'}}>
+      <Row >
+        <CreateRumour/>
+      </Row>
+    </Container>
+  );
+}
+ 
+export default MainComponent;
---
+++ rumorz/src/Components/RumourFeed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container,Row,Col} from 'react-bootstrap';
+import './Styling/RumourFeed.css'
+function RumourFeed() {
+  return(
+    <div className='RumourFeed padding'>
+      Rumour Feed will go here
+    </div>
+  );
+}
+ 
+export default RumourFeed;
